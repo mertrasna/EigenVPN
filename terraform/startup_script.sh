4e8ee86c60ed61ae4575497a737a7ddfc3df203e
Mert Rasna <mrasnalu02@gmail.com>
2026-03-15 15:44:58 +0100

    feat: user_data script nat rules

--- terraform/startup_script.sh
+++ terraform/startup_script.sh
@@ -18,12 +18,22 @@ Address = 10.0.0.1/24
 ListenPort = 51820
 
 [Peer]
-PublicKey = ${client_public_key} # reads the terraform variable
+PublicKey = ${client_public_key} 
 AllowedIPs = 10.0.0.2/32
 EOF
 
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p
+
+# NAT - forward traffic to internet
+iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -o ens5 -j MASQUERADE
+iptables -A FORWARD -i wg0 -j ACCEPT
+iptables -A FORWARD -o wg0 -j ACCEPT
+
+# Save iptables rules so they persist on reboot
+DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent
+netfilter-persistent save
+
 
 # Enable and start WireGuard
 systemctl enable wg-quick@wg0
